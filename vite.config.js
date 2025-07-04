ab50c39056d095038361ac17ad540bd844585aef
Luigi <faizan@genealpha.ai>
2025-07-04 11:17:25 +0500

    Add's jest testing library and sets up project to use process.env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import EnvironmentPlugin from "vite-plugin-environment";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [EnvironmentPlugin("all"), react(), tailwindcss()],
 });
